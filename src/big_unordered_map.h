46d6e43eba7666e51a9ef8a71802c014d00b09fa
Junhao Li <jl2922@cornell.edu>
2017-04-11 11:40:05 -0400

    map pt dets according to mem available.

--- src/big_unordered_map.h
+++ src/big_unordered_map.h
@@ -35,6 +35,7 @@ class BigUnorderedMap {
   void complete_async_inc_trunk();  // Used by async inc when buf full.
   void set_skeleton(const std::pair<K, V>&);  // Called at the beginning.
   void reset_cnts();  // Reset send and recv counts.
+  void set_node_buckets();
   int proc_id;
   int n_procs;
   std::unordered_map<K, V, H> local_map;
@@ -53,7 +54,10 @@ class BigUnorderedMap {
   std::vector<unsigned long long> recv_cnts;
   std::vector<unsigned long long> recv_totals;
   std::vector<unsigned long long> recv_trunk_totals;
-  enum { TAG_KV, TAG_TRUNK_FINISH, TAG_FINISH };
+  enum { TAG_NODES, TAG_KV, TAG_TRUNK_FINISH, TAG_FINISH };
+  int total_node_buckets;
+  int local_node_buckets;
+  std::vector<int> node_map;
 };
 
 template <class K, class V, class H>
@@ -70,6 +74,7 @@ BigUnorderedMap<K, V, H>::BigUnorderedMap(
   buf_send_content.resize(buf_size);
   reset_cnts();
   this->set_skeleton(skeleton);
+  this->set_node_buckets();
 }
 
 template <class K, class V, class H>
@@ -243,5 +248,80 @@ void BigUnorderedMap<K, V, H>::reset_cnts() {
 template <class K, class V, class H>
 void BigUnorderedMap<K, V, H>::all_combine(
     std::unordered_map<K, V, H>& local_map, boost::mpi::communicator& world) {}
+
+template <class K, class V, class H>
+void BigUnorderedMap<K, V, H>::set_node_buckets() {
+  // Obtain local node buckets. -1 if not available.
+  std::ifstream nodes_file("NODES");
+  std::string local_proc_name = boost::mpi::environment::processor_name();
+  std::string proc_name_in;
+  int proc_mem_in;
+  local_node_buckets = 0;
+  while (nodes_file >> proc_name_in) {
+    nodes_file >> proc_mem_in;
+    if (proc_name_in == local_proc_name) local_node_buckets = proc_mem_in;
+    break;
+  }
+  std::list<boost::mpi::request> reqs;
+  for (int i = 0; i < n_procs; i++) {
+    if (i == proc_id) continue;
+    reqs.push_front(world->isend(i, TAG_NODES, local_node_buckets));
+  }
+
+  // Receive node buckets info from remote nodes.
+  int recv_cnt = 0;
+  std::vector<int> node_buckets(n_procs, 0);
+  node_buckets[proc_id] = local_node_buckets;
+  while (recv_cnt < n_procs - 1) {
+    const auto& status = world->probe(boost::mpi::any_source, TAG_NODES);
+    recv_cnt++;
+    const int source = status.source();
+    world->recv(source, TAG_NODES, node_buckets[source]);
+  }
+  wait_all(reqs.begin(), reqs.end());
+
+  // Setup node map.
+  const bool all_unknown =
+      std::all_of(node_buckets.begin(), node_buckets.end(), [](const int i) {
+        return i == 0;
+      });
+  if (all_unknown) {
+    // No node infomation available, distribute evenly.
+    total_node_buckets = n_procs;
+    local_node_buckets = 1;
+    node_map.reserve(n_procs);
+    for (int i = 0; i < n_procs; i++) node_map.push_back(i);
+  } else {
+    int min_node_buckets = std::numeric_limits<int>::max();
+    for (int i = 0; i < n_procs; i++) {
+      if (node_buckets[i] > 0 && node_buckets[i] < min_node_buckets) {
+        min_node_buckets = node_buckets[i];
+      }
+    }
+    total_node_buckets = 0;
+    for (int i = 0; i < n_procs; i++) {
+      if (node_buckets[i] == 0) node_buckets[i] = min_node_buckets;
+      total_node_buckets += node_buckets[i];
+    }
+    local_node_buckets = node_buckets[proc_id];
+    node_map.reserve(total_node_buckets);
+    for (int i = 0; i < n_procs; i++) {
+      node_map.insert(node_map.end(), node_buckets[i], i);
+    }
+  }
+
+  if (proc_id == 0) {
+    printf("node_map: ");
+    for (const auto i : node_map) printf("%d ", i);
+    printf("\n");
+  }
+  printf(
+      "Node #%d (%s) stores: %.2f %% pt_dets\n",
+      proc_id,
+      local_proc_name.c_str(),
+      local_node_buckets * 100.0 / total_node_buckets);
+  world->barrier();
+  exit(0);
+}
 
 #endif  // BIG_UNORDERED_MAP_H_
