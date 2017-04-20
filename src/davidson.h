0707e4d61498bf00ace0f9af68886e5fc6e1ee16
Junhao Li <jl2922@cornell.edu>
2017-04-20 18:32:34 -0400

    implement davidson.

--- src/davidson.h
+++ src/davidson.h
@@ -3,41 +3,152 @@
 
 #include <Eigen/Dense>
 #include <functional>
+#include <vector>
 
+// Translated from Adam's fortran code.
 class Davidson {
  public:
-  Davidson(std::function<double(int, int)>& getHamiltonian, const int n) {
-    this->getHamiltonian = &getHamiltonian;
+  Davidson(
+      std::function<double(int, int)>& get_hamiltonian_elem,
+      std::function<std::vector<double>(std::vector<double>)>& apply_hamiltonian,
+      const int n) {
+    this->get_hamiltonian_elem = &get_hamiltonian_elem;
+    this->apply_hamiltonian = &apply_hamiltonian;
     this->n = n;
+    diagonalized = false;
   }
 
-  double get_lowest_eigenvalue();
+  void set_initial_vector(const std::vector<double>& vector) { this->initial_vector = vector; }
+
+  void diagonalize();
+
+  double get_lowest_eigenvalue() {
+    if (!diagonalized) diagonalize();
+    return lowest_eigenvalue;
+  }
+
+  const std::vector<double>& get_lowest_eigenvector() {
+    if (!diagonalized) diagonalize();
+    return lowest_eigenvector;
+  }
 
  private:
-  std::function<double(int, int)>* getHamiltonian;
+  // Use functional programming to allow either direct or indirect evaluation.
+  std::function<double(int, int)>* get_hamiltonian_elem;
+  std::function<std::vector<double>(std::vector<double>)>* apply_hamiltonian;
+
+  // Solutions.
+  double lowest_eigenvalue;
+  std::vector<double> lowest_eigenvector;
+
+  std::vector<double> initial_vector;
+
+  // Length in each direction.
   int n;
+
+  bool diagonalized;
 };
 
-double Davidson::get_lowest_eigenvalue() {
-  Eigen::MatrixXd matrix(n, n);
+void Davidson::diagonalize() {
+  if (n == 1) {
+    lowest_eigenvalue = (*get_hamiltonian_elem)(0, 0);
+    lowest_eigenvector = std::vector<double>(1, 1.0);
+  }
 
-  for (int i = 0; i < n; i++) {
+  const int iterations = std::min(n, 50);
+  double lowest_eigenvalue = 0.0;
+  double lowest_eigenvalue_prev = 0.0;
+  double residual_norm = 0.0;
+
+  Eigen::MatrixXd v = Eigen::MatrixXd::Zero(n, iterations);
+
+  if (initial_vector.size() != n) {
+    v(0, 0) = 1.0;  // Start from HF.
+  } else {
+    for (int i = 0; i < n; i++) v(i, 0) = initial_vector[i];
+    v.col(0).normalize();
+  }
+
+  Eigen::MatrixXd Hv = Eigen::MatrixXd::Zero(n, iterations);
+  Eigen::VectorXd w = Eigen::VectorXd::Zero(n);  // Lowest eigenvector so far.
+  Eigen::VectorXd Hw = Eigen::VectorXd::Zero(n);
+  Eigen::MatrixXd h_krylov = Eigen::MatrixXd::Zero(iterations, iterations);
+  Eigen::MatrixXd h_overwrite = Eigen::MatrixXd::Zero(iterations, iterations);
+  Eigen::VectorXd eigenvalues = Eigen::VectorXd::Zero(iterations);
+  int len_work = 3 * iterations - 1;
+  Eigen::VectorXd work(len_work);
+  bool converged = false;
+  std::vector<double> tmp_v(n);
+
+  // Get diagonal elements.
+  Eigen::VectorXd diag_elems(n);
+  for (int i = 0; i < n; i++) diag_elems[i] = (*get_hamiltonian_elem)(i, i);
+
+  // First iteration.
+  for (int i = 0; i < n; i++) tmp_v[i] = v(i, 0);
+  const auto& tmp_Hv = (*apply_hamiltonian)(tmp_v);
+  for (int i = 0; i < n; i++) Hv(i, 0) = tmp_Hv[i];
+  lowest_eigenvalue = v.col(0).dot(Hv.col(0));
+  h_krylov(0, 0) = lowest_eigenvalue;
+  w = v.col(0);
+  Hw = Hv.col(0);
+  printf("Initial iteration. Eigenvalue: %.10f\n", lowest_eigenvalue);
+
+  residual_norm = 1.0;  // So at least one iteration is done.
+  int n_iter = std::min(n, iterations + 1);
+  int n_diagonalize = 1;
+
+  for (int it = 1; it < n_iter; it++) {
+    // Compute residual.
     for (int j = 0; j < n; j++) {
-      matrix(i, j) = (*getHamiltonian)(i, j);
+      v(j, it) = (Hw(j, 0) - lowest_eigenvalue * w(j, 0)) / (lowest_eigenvalue - diag_elems(j));
+      if (fabs(lowest_eigenvalue - diag_elems[j]) < 1.0e-8) v(j, it) = -1.0;
     }
+
+    // If residual is small, converge.
+    residual_norm = v.col(it).norm();
+    if (residual_norm < 1.0e-12) converged = true;
+
+    // Orthogonalize and normalize.
+    for (int i = 0; i < it; i++) {
+      double norm = v.col(it).dot(v.col(i));
+      v.col(it) -= norm * v.col(i);
+    }
+    v.col(it).normalize();
+
+    // Apply H once.
+    for (int i = 0; i < n; i++) tmp_v[i] = v(i, it);
+    const auto& tmp_Hv2 = (*apply_hamiltonian)(tmp_v);
+    for (int i = 0; i < n; i++) Hv(i, it) = tmp_Hv2[i];
+
+    // Construct Krylow matrix and diagonalize.
+    for (int i = 0; i <= it; i++) {
+      h_krylov(i, it) = v.col(i).dot(Hv.col(it));
+      h_krylov(it, i) = h_krylov(i, it);
+    }
+    len_work = 3 * it + 2;
+    h_overwrite = h_krylov;
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eigenSolver(h_overwrite);
+    const auto& eigenvalues = eigenSolver.eigenvalues();
+    lowest_eigenvalue = eigenvalues.minCoeff();
+    w = v.leftCols(it) * h_overwrite.col(0).topRows(it);
+    Hw = Hv.leftCols(it) * h_overwrite.col(0).topRows(it);
+
+    if (it > 1 && fabs(lowest_eigenvalue - lowest_eigenvalue_prev) < 1.0e-6) {
+      converged = true;
+      break;
+    } else {
+      lowest_eigenvalue_prev = lowest_eigenvalue;
+      n_diagonalize++;
+      printf("Iteration #%d. Eigenvalue: %.10f\n", n_diagonalize, lowest_eigenvalue);
+    }
+    if (converged) break;
   }
 
-  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eigenSolver(matrix);
-  const auto eigenvalues = eigenSolver.eigenvalues();
-  
-  double lowest_eigenvalue = eigenvalues[0];
-  for (int i = 1; i < eigenvalues.size(); i++) {
-    if (eigenvalues[i] < lowest_eigenvalue) {
-      lowest_eigenvalue = eigenvalues[i];
-    }
-  }
-
-  return lowest_eigenvalue;
+  this->lowest_eigenvalue = lowest_eigenvalue;
+  lowest_eigenvector.resize(n);
+  for (int i = 0; i < n; i++) lowest_eigenvector[i] = w(i);
+  diagonalized = true;
 }
 
 #endif
--- src/davidson_test.cc
+++ src/davidson_test.cc
@@ -2,28 +2,60 @@
 
 #include <cstdio>
 #include <functional>
+#include <list>
 
+// Test with a Hilbert matrix.
 class TestHamiltonian {
  public:
-  TestHamiltonian(int gamma) { this->gamma = gamma; }
-  double get(int i, int j) {
+  TestHamiltonian(int n, int gamma) {
+    this->n = n;
+    this->gamma = gamma;
+  }
+
+  double get_hamiltonian(int i, int j) {
     if (i == j) return -1.0 / (2 * i + 1);
     return -1.0 / gamma / (i + j + 1);
   }
 
+  std::vector<double> apply_hamiltonian(const std::vector<double>& v) {
+    std::vector<double> Hv(n, 0.0);
+    for (int i = 0; i < n; i++) {
+      Hv[i] += get_hamiltonian(i, i) * v[i];
+      for (int j = i + 1; j < n; j++) {
+        double h_ij = get_hamiltonian(i, j);
+        Hv[i] += h_ij * v[j];
+        Hv[j] += h_ij * v[i];
+      }
+    }
+    return Hv;
+  }
+
  private:
+  int n;
   int gamma;
 };
 
 int main(int argc, char** argv) {
-  int N = 10;
-  TestHamiltonian hamiltonian(N);
+  int N = 10000;
+  TestHamiltonian hamiltonian(N, 10);
 
-  std::function<double(int, int)> getHamiltonian =
-      std::bind(&TestHamiltonian::get, &hamiltonian, std::placeholders::_1, std::placeholders::_2);
+  std::function<double(int, int)> get_hamiltonian = std::bind(
+      &TestHamiltonian::get_hamiltonian,
+      &hamiltonian,
+      std::placeholders::_1,
+      std::placeholders::_2);
 
-  Davidson davidson(getHamiltonian, N);
+  std::function<std::vector<double>(std::vector<double>)> apply_hamiltonian =
+      std::bind(&TestHamiltonian::apply_hamiltonian, &hamiltonian, std::placeholders::_1);
+
+  Davidson davidson(get_hamiltonian, apply_hamiltonian, N);
 
   printf("Lowest Eigenvalue: %.10f\n", davidson.get_lowest_eigenvalue());
+
+  const auto& eigenvector = davidson.get_lowest_eigenvector();
+
+  printf("Top elements in eigenvector:\n");
+  for (int i = 0; i < 5; i++) printf("%d: %.10f\n", i, eigenvector[i]);
+
   return 0;
 }
